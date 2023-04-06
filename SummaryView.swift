3319c279326ac9e8297549d263d14ea396651038
Dylan Domeracki <ddomeracki4103@Stu.d214.org>
2023-04-06 14:28:48 -0500

    Created Sentence

---
+++ SummaryView.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryView.swift
+//  GitHubPractice2
+//
+//  Created by Dylan Domeracki on 4/6/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text("The adjective noun likes to verb ")
+
+        }
+    }
+
