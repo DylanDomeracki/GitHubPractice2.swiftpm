bb290a1ddf506f7359b3b9588820d77135d0918c
Dylan Domeracki <ddomeracki4103@Stu.d214.org>
2023-04-06 14:38:33 -0500

    Made @State of type MadLib, Created Textfield and Passed Madly object to overview

--- NounView.swift
+++ NounView.swift
@@ -1,14 +1,15 @@
 import SwiftUI
 
 struct NounView: View {
+    @State var madlib: MadLib
     var body: some View {
         NavigationView {
             NavigationLink {
-                VerbView()
+                VerbView(madlib: $madlib)
             } label: {
                 Text("To Noun View")
-            }
-
+                }
+            TextField("Noun", text: madlib.noun)
         }
     }
 }
